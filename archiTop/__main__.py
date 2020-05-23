b52fffbf0035bcecd5b3025e8b53f8bca6744612
Julian-Brendel <julianbrendel99@gmail.com>
2020-05-23 18:13:39 +0100

    Add basic cli interactivity

--- archiTop/__main__.py
+++ archiTop/__main__.py
@@ -1,14 +1,24 @@
-from archidekt import get_archidekt_deck
-from deck_construction import construct_deck
+import argparse
 import json
 
-deck_id = 72608
+from archidekt import get_archidekt_deck
+from config import default_deck_name
+from deck_construction import construct_deck
 
+# parse input
+parser = argparse.ArgumentParser(description='Convert archidekt deck to TableTop')
+parser.add_argument('deckID',
+                    help='Archidekt deck-ID to convert')
+parser.add_argument("--name", type=str,
+                    help="Deckname")
+args = parser.parse_args()
+
+deck_name = args.name if args.name else default_deck_name
 
 # fetch deck data, (name and images of cards)
-cards = get_archidekt_deck(deck_id)
+cards = get_archidekt_deck(args.deckID)
 
 deck = construct_deck(cards)
 
 # save deck
-json.dump(deck, open('deck.json', 'w'))
+json.dump(deck, open(f'{deck_name}.json', 'w'))
---
+++ archiTop/config.py
@@ -0,0 +1,6 @@
+"""Config-file for archiTop package"""
+
+
+default_deck_name = 'mtgDeck'
+# todo: add windows folder path and auto detection
+mac_os_table_top_saves = '~/Library/Tabletop Simulator/Saves/Saved Objects/'
